c1f5709b91d17feffb837da1e988ba30a761f0cb
Noath Muñoz <91708074+DR-MAKA@users.noreply.github.com>
2021-12-06 15:09:53 -0500

    Add files via upload

---
+++ IDE/hola mundo.py
@@ -0,0 +1,6 @@
+# Range es el rango para que el elemto x lo recorra 
+# El cuerpo del bucle (print("hola mundo")) se repetira el numero de veces que el rango se lo indioque en este caso
+#el rango va desde 0 hasta el 1000, una vez llegue al numero 1000 de repeticiones el ciclo terminara  
+
+for x in range(0,1000):
+  print("hola mundo")
